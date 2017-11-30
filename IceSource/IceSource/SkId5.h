0b02d22c460b2370a479ff61d577b0120742731b
Rover656 <Rover656@users.noreply.github.com>
2017-11-30 09:45:22 +0000

    Update SkId5.h

--- IceSource/IceSource/SkId5.h
+++ IceSource/IceSource/SkId5.h
@@ -1,24 +1,29 @@
 #pragma once
-
+//Get address
 inline long SKID(long SkId) {
 	return (SkId + (DWORD)GetModuleHandle(0));
 }
-
+//Get address (Use this if you use the #addys channel or find them yourself)
+inline long SKIDAddr(long SkId) {
+	return (SkId + (DWORD)GetModuleHandle(0) - 0x400000);
+}
+//Memory
 namespace SkId {
+	//Compare addys
 	bool SkIdpare(const BYTE *pSkId, const BYTE *bSkId, const char *szSkId)
 	{
 		for (; *szSkId; ++szSkId, ++pSkId, ++bSkId)
 			if (*szSkId == 'x' && *pSkId != *bSkId) return 0;
 		return (*szSkId) == NULL;
 	}
-
+	//Find patterns
 	DWORD FindPattern(DWORD dwSkId, DWORD dwLenSkId, BYTE *bSkId, char *szSkId)
 	{
 		for (int i = 0; i<(int)dwLenSkId; i++)
 			if (SkIdpare((BYTE*)(dwSkId + (int)i), bSkId, szSkId))  return (int)(dwSkId + i);
 		return 0;
 	}
-
+	//scan addys
 	long Scan(char* SkId)
 	{
 		MEMORY_BASIC_INFORMATION meme;
